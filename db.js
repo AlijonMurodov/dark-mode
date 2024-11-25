20c137dc1ede2898e27e03a85762be9bda959693
ALIJ0N <alijonmurodov10@gmail.com>
2024-11-25 15:24:06 +0300

    add data to sqlite

--- db.js
+++ db.js
@@ -1,24 +1,39 @@
 const sqlite3 = require('sqlite3').verbose();
-
+function writeDb(){}
+function readDb(){}
 function getData(callback) {
-  const db = new sqlite3.Database('testt.db', (err) => {
+  const db = new sqlite3.Database('test.db', (err) => {
     if (err) {
       console.error('Error opening database:', err);
       callback(err);
       return;
     }
   });
-  db.all('SELECT * FROM test', (err, rows) => {
+
+  // Create table if it does not exist
+  db.run(`CREATE TABLE IF NOT EXISTS test (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+  )`, (err) => {
     if (err) {
-      console.error('Error querying data:', err);
+      console.error('Error creating table:', err);
       callback(err);
-    } else {
-      callback(null, rows);
+      return;
     }
-    db.close((err) => {
+
+    // Query data from the table
+    db.all('SELECT * FROM test', (err, rows) => {
       if (err) {
-        console.error('Error closing database:', err);
+        console.error('Error querying data:', err);
+        callback(err);
+      } else {
+        callback(null, rows);
       }
+      db.close((err) => {
+        if (err) {
+          console.error('Error closing database:', err);
+        }
+      });
     });
   });
 }
